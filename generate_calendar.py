2e6b9bfd090975509fc180036e5e17242d7058d6
Matthew Pick <matt@matthewpick.com>
2023-12-10 13:08:04 -0600

    back-calculate by one year

--- generate_calendar.py
+++ generate_calendar.py
@@ -1,6 +1,7 @@
 import calendar
 from datetime import date, timedelta
 
+
 def last_weekday_of_month(year, month):
     _, last_day = calendar.monthrange(year, month)
     last_day_of_month = date(year, month, last_day)
@@ -10,15 +11,20 @@ def last_weekday_of_month(year, month):
 
     return last_day_of_month
 
+
 def calculate_last_weekday_for_next_two_years():
     current_year = date.today().year
+    start_year = current_year - 1
 
-    for year in range(current_year, current_year + 2):
+    for year in range(start_year, current_year + 2):
         for month in range(1, 13):
-            last_weekday = last_weekday_of_month(year, month)
-            weekday_name = calendar.day_name[last_weekday.weekday()]
-            # print(f"Last weekday of {calendar.month_name[month]} {year}: {last_weekday.strftime('%Y-%m-%d')} ({weekday_name})")
-            yield last_weekday
+            yield last_weekday_of_month(year, month)
+
 
 if __name__ == "__main__":
-    calculate_last_weekday_for_next_two_years()
+    for last_weekday in calculate_last_weekday_for_next_two_years():
+        year = last_weekday.year
+        month = last_weekday.month
+        weekday_name = calendar.day_name[last_weekday.weekday()]
+        print(f"Last weekday of {calendar.month_name[month]} {year}: "
+              f"{last_weekday.strftime('%Y-%m-%d')} ({weekday_name})")
